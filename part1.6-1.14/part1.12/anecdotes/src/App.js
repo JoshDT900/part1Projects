39b28579dbd4e478340b60b27fe999ba0ef93198
JoshDT900 <djtroutman900@gmail.com>
2022-04-02 14:04:40 -0400

    Final version

---
+++ part1.6-1.14/part1.12/anecdotes/src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Main = ({text}) => <h1>{text}</h1>;
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Anecdote = ({ anecdote }) => <h2>{anecdote}</h2>
+const Votes = ({ votes }) => <h4>Current votes: {votes}</h4>
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]  
+
+  let stateArr = new Array(anecdotes.length).fill(0)
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(stateArr)  
+
+  const getRandomAnecdote = () => {    
+    setSelected(Math.floor(Math.random(selected) * anecdotes.length))    
+  }
+
+  const handleVotes = () => {
+    const votesCopy = [...votes]
+    votesCopy[selected]++
+    setVotes(votesCopy)
+  }  
+
+  const highestVotes = Math.max(...votes);
+  const winningAnecdote = anecdotes[votes.indexOf(highestVotes)];
+  
+
+  return (
+    <div>
+      <Main text="Random Anecdotes"/>
+      <Anecdote anecdote={anecdotes[selected]}/>
+      <Votes votes={votes[selected]} />
+      <Button onClick={handleVotes} text="Vote"/>
+      <Button onClick={getRandomAnecdote} text="Randomize Anecdote"/>
+      <Anecdote anecdote={winningAnecdote}/>
+      <Votes votes={highestVotes} />            
+    </div>
+  )
+}
+
+export default App
